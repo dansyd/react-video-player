29dd0d30b2d4ccf0ee7db38e64d967f5d4ca7e51
Daniele Peviani <daniele.peviani@gmail.com>
2017-03-24 18:16:18 +1100

    Added video list, video list item and event handlers

--- src/index.js
+++ src/index.js
@@ -2,24 +2,39 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyB0aCh9oyj-dFbdj4ZXRfbEfNXbhROi1Zs';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos }); // ES6 for this.setState({ videos: videos }); ONLY WHEN SAME NAME
+    this.videoSearch('surfboard');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      }); // ES6 for this.setState({ videos: videos }); ONLY WHEN SAME NAME
     });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={ this.state.selectedVideo }/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={ this.state.videos } />
       </div>
     );
   }
